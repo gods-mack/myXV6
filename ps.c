887b95872833ee6f771b839e7c48c1e0c87beaf9
gods-mack <sehorem@gmail.com>
2020-04-06 00:20:03 +0530

    update: ps command add suport for printing PPID

---
+++ ps.c
@@ -0,0 +1,15 @@
+#include "types.h"
+#include "stat.h"
+
+
+#include "user.h"
+
+#include "fcntl.h"
+
+
+int main(int argc, char *argv[]){
+	//call cps func which is defined in proc.c
+	return cps(); 
+
+	exit();
+}
